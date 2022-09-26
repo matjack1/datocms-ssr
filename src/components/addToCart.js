2bded8ca85cdc6f6cf2fa33abedfe2a0beba7109
Nicola Lazzaroni <nicola.lazzaroni@gmail.com>
2022-09-26 09:16:12 +0200

    Reverse to external price

--- src/components/addToCart.js
+++ src/components/addToCart.js
@@ -53,7 +53,7 @@ const AddToCart = ({ sku, quantity }) => {
       order: cl.orders.relationship(cart.id),
       item: cl.skus.relationship(sku.id),
       _update_quantity: true,
-      _external_price: false,
+      _external_price: true,
     };
 
     const handleError = (e) => {
