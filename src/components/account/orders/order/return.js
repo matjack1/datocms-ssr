319cea2c5d08ff110c8b1bc1f4bcdb534a174141
emkeythekeyem <asoadrian@gmail.com>
2022-10-12 12:30:07 +0200

    changed email + cliente name & id

--- src/components/account/orders/order/return.js
+++ src/components/account/orders/order/return.js
@@ -38,6 +38,7 @@ const CustomerOrderReturn = () => {
   const [success, setSuccess] = useState(null);
   const [formData, setFormData] = useState({
     customer: "",
+    customerId: "",
     message: "",
     order: "",
     products: [],
@@ -69,6 +70,7 @@ const CustomerOrderReturn = () => {
 
   const handleReturnMail = async (event) => {
     const data = formData;
+    console.log("customer",formData)
 
     axios
       .post("/.netlify/functions/returnMail", data)
@@ -94,6 +96,7 @@ const CustomerOrderReturn = () => {
       const nextFormState = {
         ...formData,
         customer: customer.email,
+        customerId : customer.metadata.full_name+" "+customer.metadata.reference
       };
       setFormData(nextFormState);
     }
--- src/components/account/support.js
+++ src/components/account/support.js
@@ -26,6 +26,7 @@ import { useForm } from "react-hook-form";
 const Support = () => {
   const [formData, setFormData] = useState({
     customer: "",
+    customerId: "",
     message: "",
   });
   const [loading, setLoading] = useState(false);
@@ -40,7 +41,6 @@ const Support = () => {
   } = useForm();
 
   const handleSupportMail = async (event) => {
-
     const data = formData;
 
     axios
@@ -62,7 +62,7 @@ const Support = () => {
   };
 
   const onUpdateField = (e) => {
-    console.log("onUpdateField",e.target.name,e.target.value)
+    console.log("onUpdateField", e.target.name, e.target.value);
     const nextFormState = {
       ...formData,
       [e.target.name]: e.target.value,
@@ -75,6 +75,7 @@ const Support = () => {
       const nextFormState = {
         ...formData,
         customer: customer.email,
+        customerId: customer.metadata.full_name + " " + customer.metadata.reference,
       };
       setFormData(nextFormState);
     }
@@ -99,7 +100,7 @@ const Support = () => {
         <Heading as="h1" variant="h2" sx={{ color: "primary" }}>
           Ciao, come posso aiutarti?
         </Heading>
-        <Grid columns={["1fr",".7fr .3fr"]} gap={[0,12]}>
+        <Grid columns={["1fr", ".7fr .3fr"]} gap={[0, 12]}>
           {success === null ? (
             <>
               <Box as="form" onSubmit={handleSubmit(handleSupportMail)}>
