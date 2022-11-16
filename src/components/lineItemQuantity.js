91e228011c2da811374e85d05590d3006be8809b
emkeythekeyem <asoadrian@gmail.com>
2022-11-16 17:19:14 +0100

    custom quantity input

--- src/components/lineItemQuantity.js
+++ src/components/lineItemQuantity.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Flex, Text } from "theme-ui";
+import { Box, Button, Flex, Input, Text } from "theme-ui";
 import { useDatoCmsSdk } from "../hooks/useDatoCmsSdk";
 
 const LineItemQuantity = ({ lineItem, quantity, updateQuantity }) => {
   const [sku, setSku] = useState();
   const [currentQuantity, setCurrentQuantity] = useState(quantity);
+  const [inputValue, setInputValue] = useState(quantity);
   const client = useDatoCmsSdk();
 
   async function run() {
@@ -22,12 +23,22 @@ const LineItemQuantity = ({ lineItem, quantity, updateQuantity }) => {
     setSku(records[0]);
   }
 
+  function handleQuantityChange(e) {
+    setInputValue(e.target.value);
+  }
+
   function addQuantity() {
-    setCurrentQuantity(currentQuantity + sku.multiple);
+    if (currentQuantity < 1000) {
+      setInputValue(parseInt(inputValue) + 1);
+      setCurrentQuantity(parseInt(currentQuantity) + 1);
+    }
   }
 
   function removeQuantity() {
-    setCurrentQuantity(currentQuantity - sku.multiple);
+    if (currentQuantity > 2) {
+      setInputValue(parseInt(inputValue) - 1);
+      setCurrentQuantity(parseInt(currentQuantity) - 1);
+    }
   }
 
   useEffect(() => {
@@ -46,44 +57,53 @@ const LineItemQuantity = ({ lineItem, quantity, updateQuantity }) => {
           alignItems: "center",
         }}
       >
-        <Text sx={{ mr: [5], fontSize: [1], color: "lightBorder" }}>
-          Quantita:
+        <Text sx={{ mr: [5], fontSize: [2], color: "lightBorder" }}>
+          Quantità:
         </Text>
         <Flex>
           <Button
+            sx={{mr:["-1px"]}}
             variant="buttons.darkEmpty"
-            disabled={currentQuantity <= sku?.multiple}
+            disabled={currentQuantity < 2}
             onClick={() => removeQuantity()}
           >
             -
           </Button>
-          <Flex
-            sx={{
-              width: "80px",
-              justifyContent: "center",
-              alignItems: "center",
-              borderTop: "1px solid",
-              borderBottom: "1px solid",
-              borderColor: "dark",
-              py: [2],
-              fontSize:[1,2]
+          <Input
+            sx={{ maxWidth: "120px", textAlign: "center" }}
+            variant="inputs.dark"
+            onChange={handleQuantityChange}
+            onBlur={(e) => {
+              if (e.target.value < 1 || isNaN(parseInt(e.target.value))) {
+                setCurrentQuantity(1);
+                setInputValue(1);
+              } else if (e.target.value > 1000) {
+                setCurrentQuantity(1000);
+                setInputValue(1000);
+              }
+              else {
+                setCurrentQuantity(
+                  e.target.value.replace(/[^0-9a-zA-Z]+/g, "")
+                );
+                setInputValue(e.target.value.replace(/[^0-9a-zA-Z]+/g, ""));
+              }
             }}
-          >
-            {currentQuantity}
-          </Flex>
+            max="1000"
+            type="tel"
+            pattern="[0-9]*"
+            inputmode="numeric"
+            defaultValue={currentQuantity}
+            value={inputValue}
+          />
           <Button
+            sx={{ml:["-1px"]}}
             variant="buttons.darkEmpty"
-            disabled={!sku}
+            disabled={currentQuantity > 1000}
             onClick={() => addQuantity()}
           >
             +
           </Button>
         </Flex>
-        <Text sx={{ pl: [2], fontSize: [1], color: "lightBorder" }}>
-          minimo {sku?.minimum}
-          <br />
-          multiplo {sku?.multiple}
-        </Text>
       </Flex>
     </Box>
   );
