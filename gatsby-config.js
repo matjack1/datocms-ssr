978b8d7346871979f0c1e5e9adaad6925d0e3f3f
Nicola Lazzaroni <nicola.lazzaroni@gmail.com>
2023-10-19 23:48:55 +0200

    last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,49 +8,5 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-theme-ui",
-    "gatsby-plugin-netlify",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /\.inline\.svg$/,
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        // You can find your read-only API token under the Settings > API tokens
-        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
-        apiToken: `7f672cb51a4f9c2dce0c59b466b8c6`,
-
-        // The project environment to read from. Defaults to the primary environment:
-        environment: `main`,
-
-        // If you are working on development/staging environment, you might want to
-        // preview the latest version of records instead of the published one:
-        previewMode: false,
-
-        // Disable automatic reloading of content when some change occurs on DatoCMS:
-        disableLiveReload: false,
-
-        // Custom API base URL (most don't need this)
-        // apiUrl: 'https://site-api.datocms.com',
-
-        // Setup locale fallbacks
-        // In this example, if some field value is missing in Italian, fall back to English
-        // localeFallbacks: {
-        //   it: ["en"],
-        // },
-      },
-    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
-import { getCustomerToken } from "@commercelayer/js-auth";
-import fetch from "node-fetch";
+import { buildClient } from "@datocms/cma-client-browser";
 
 const IndexPage = () => {
   return (
